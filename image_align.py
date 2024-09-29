7667b6bf25534f6509f7252c25b34345206dad5d
Sameer Shroff <ssmeer1988@gmail.com>
2024-09-28 21:38:16 -0400

    refactor 2

--- image_align.py
+++ image_align.py
@@ -13,11 +13,37 @@ class ImageAlign():
 
         self.working_dir = working_dir
         self.image_ext = image_ext
+        self.df_images = self.get_image_status()
 
     def __str__(self):
         [print(f'{x} : {getattr(self, x)}') for x in dir(self) if not str.startswith(x, '__')]
         return ''
+    
+        
+    def get_image_status(self):
+
+        image_dir = self.working_dir
+        image_ext = self.image_ext
+
+        image_glob = os.path.join(image_dir, f'*{image_ext}')
+        images = glob.glob(image_glob, recursive=True)
+        print(f'{len(images)} {image_ext} images found')
+
+        csv_glob = os.path.join(image_dir, f'*.csv')
+        csvs = glob.glob(csv_glob, recursive=True)
+        print(f'{len(csvs)} annotation .csv files found')
+
+        def process_images(fullpath):
+            basename = os.path.basename(fullpath)
+            has_annotations =  os.path.isfile(f'{fullpath}.csv')
+            d = {'fullpath':fullpath, 'basename': basename, 'has_annotations': has_annotations}
+            return d
+
+        d = [process_images(x) for x in images]
+        df_images = pd.DataFrame(d)
+
+        return df_images
 
 
-session = ImageAlign('.', '.png')
+session = ImageAlign('.\images', '.png')
 print(session)
