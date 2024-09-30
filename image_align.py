ad542c1f7d7fe13741c4aa333d94924cf06c4b8d
Sameer Shroff <ssmeer1988@gmail.com>
2024-09-29 20:06:43 -0400

    updated

--- image_align.py
+++ image_align.py
@@ -7,6 +7,8 @@ import os
 import tkinter as tk
 import glob
 
+matplotlib.use('Qt5Agg')
+
 class ImageAlign():
 
     def __init__(self, working_dir, image_ext = '.png'):
@@ -15,12 +17,13 @@ class ImageAlign():
         self.image_ext = image_ext
         self.df_images = self.get_image_status()
 
+
     def __str__(self):
         #[print(f'{x} : {getattr(self, x)}') for x in dir(self) if not str.startswith(x, '__')]
         print('\n\n____________________________________________________________')
         for key, value in self.__dict__.items():
             if type(value) == pd.DataFrame:
-                print(f' * {key} :\n{value}')
+                print(f' * {key} :\n{value}\n')
             else:
                 print(f' * {key} : {value}')
         print('____________________________________________________________')
@@ -50,7 +53,43 @@ class ImageAlign():
         df_images = pd.DataFrame(d)
 
         return df_images
+    
+
+    def initialize_plot(self):
+        
+        fig = matplotlib.pyplot.figure() 
+        ax_image = fig.add_subplot()
+        ax_image.set_xlabel('[left click] to add point, [right click] to remove point')
+        
+        self.fig = fig
+        self.ax_image = ax_image
+        fig.canvas.draw()
+
+        return self
+    
+    def plot_image(self, idx):
+
+        fullpath = self.df_images.loc[idx, 'fullpath']
+        csv_fn = f'{fullpath}.csv'
+        img = cv2.imread(fullpath)
+
+        self.fig.suptitle(fullpath)
+
+        if os.path.isfile(csv_fn):
+            df_xy = pd.read_csv(csv_fn, header = 0, index_col = 0)
+        else:
+            df_xy = pd.DataFrame(columns = ['x', 'y', 'user', 'datetime', 'filename'])
+
+        self.ax_image.imshow(img)
+        #hpoints = plot_points(df_xy)
+        self.ax_image.set_title(fullpath)
+        #print(f'loaded {file}')
+        self.fig.canvas.draw()
 
 
 session = ImageAlign(r'.\images', r'.png')
+
+session.initialize_plot()
+session.plot_image(0)
 print(session)
+plt.show()
